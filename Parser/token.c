e3c0bb115ee47d8ed0331f67cfe5247807d7b7c6
pxeger <_@pxeger.com>
2021-06-17 10:15:49 +0100

    regenerate tokensation code

--- Parser/token.c
+++ Parser/token.c
@@ -60,6 +60,7 @@ const char * const _PyParser_TokenNames[] = {
     "RARROW",
     "ELLIPSIS",
     "COLONEQUAL",
+    "Y_EXC_MISHANDLING",
     "OP",
     "AWAIT",
     "ASYNC",
@@ -94,6 +95,7 @@ PyToken_OneChar(int c1)
     case '<': return LESS;
     case '=': return EQUAL;
     case '>': return GREATER;
+    case '?': return Y_EXC_MISHANDLING;
     case '@': return AT;
     case '[': return LSQB;
     case ']': return RSQB;
